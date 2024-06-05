3a4a6a15f43a4958f14149ea96967db094b2a2b6
Jmarc13 <jmarcparagas.doc@gmail.com>
2024-06-05 09:58:10 +0200

    Create class person and player

---
+++ futbol/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace futbol
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public double height { get; set; }
+        public double weight { get; set; }
+
+        public Person(string name, string lastName, int age, double height, double weight)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"Persona: {name} {lastName} | Edad: {age} | Altura: {height} | Peso: {weight}";
+        }
+    }
+}
